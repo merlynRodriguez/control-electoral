d246cc4d44e02abcf576ed3a779e98e50ba7f09d
merlynRodriguez <merlynrodriguezlafuente@gmail.com>
2026-03-06 21:08:18 -0400

    Fix typescript error en Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../services/supabase';
+
 
 export default function Login() {
   const [email, setEmail] = useState('');
